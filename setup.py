4ae31ecfc85df87f6f0adf718049efe4a1991619
seokgyun <tjrrbs1116@gmail.com>
2023-03-23 18:26:51 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from glob import glob
+import os
+
+
+
+from setuptools import setup
+
+package_name = 'command'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kim',
+    maintainer_email='tjrrbs1116@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'command_1 = command.command_1:main'
+        ],
+    },
+)
